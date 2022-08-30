2ce9c9d2e5fe56d297ef832ed2b154d02cfcdf24
ro-ze1106 <roze56477@gmail.com>
2022-08-31 06:41:33 +0900

    ルーティングページ作成

---
+++ frontend/app/src/components/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { FC, memo } from "react";
+
+export const Home: FC = memo(() => 
+  <p>HOME</p>
+);
+
+export default Home;
---
+++ frontend/app/src/components/pages/Setting.tsx
@@ -0,0 +1,7 @@
+import { FC, memo } from "react";
+
+export const Setting: FC = memo(() => 
+  <p>HOME</p>
+);
+
+export default Setting;
