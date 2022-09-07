0d4945681a8f111cedfd26fe638727da2c722c35
ro-ze1106 <roze56477@gmail.com>
2022-09-07 18:30:15 +0900

    ヘッダー作成

--- frontend/app/src/router/Router.tsx
+++ frontend/app/src/router/Router.tsx
@@ -5,14 +5,17 @@ import { About } from '../components/pages/About';
 import { Home } from '../components/pages/Home';
 import { Page404 } from '../components/pages/Page404';
 import { Setting } from '../components/pages/Setting';
+import { HeaderLayout } from '../components/templates/HerderLayout';
 
 export const Router: FC = memo(() => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/setting" element={<Setting />} />    
-    <Route path="/*" element={<Page404 />} />    
-  </Routes>
+  <HeaderLayout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/setting" element={<Setting />} />
+      <Route path="/*" element={<Page404 />} />
+    </Routes>
+  </HeaderLayout>
 ));
 
 export default Router;
