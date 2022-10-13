49066f3a9f6a433f18d1ac4cf62004720a9ac1a1
ro-ze1106 <roze56477@gmail.com>
2022-10-13 18:58:13 +0900

    本番環境用のドメイン設定変更

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4000", "https://web.today-partner.tech"
+    origins "http://localhost:4000", "https://web.today-partner.click"
 
     resource "*",
       headers: :any,
