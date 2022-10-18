ce57a7c214e2ee047c03e954c8972dc4a651a676
ro-ze1106 <roze56477@gmail.com>
2022-10-18 16:00:11 +0900

    devise_token_auth設定追加

--- backend/config/initializers/devise_token_auth.rb
+++ backend/config/initializers/devise_token_auth.rb
@@ -5,13 +5,13 @@ DeviseTokenAuth.setup do |config|
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
   # each request.
-  # リクエスト毎にトークを更新するか
-  # config.change_headers_on_each_request = false
+  # リクエスト毎にトークンを更新するか
+  config.change_headers_on_each_request = false
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
   # トークンの有効期限
-  # config.token_lifespan = 2.weeks
+  config.token_lifespan = 2.weeks
 
   # Limiting the token_cost to just 4 in testing will increase the performance of
   # your test suite dramatically. The possible cost value is within range from 4
@@ -49,7 +49,8 @@ DeviseTokenAuth.setup do |config|
                           :'client' => 'client',
                           :'expiry' => 'expiry',
                           :'uid' => 'uid',
-                          :'token-type' => 'token-type' }
+                          :'token-type' => 'token-type',
+                          authorization: "authorization"  }
 
   # Makes it possible to use custom uid column
   # config.other_uid = "foo"
